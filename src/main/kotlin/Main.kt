dda422545ef02848968e90d170b94110471c24b0
danielgabler <gabler-eventmanagement@gmx.de>
2023-10-17 10:49:43 +0200

    Vorname Nachname Challenge

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,10 @@
+fun main(args: Array<String>) {
+    println("Schreib hier bitte deinen Vornamen")
+    var Vorname = readln()
+    println("Schreib hier bitte deinen Nachnamen")
+    var Nachname = readln()
+    println("Daniel Gabler")
+    // Try adding program arguments via Run/Debug configuration.
+    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
+    println("Program arguments: ${args.joinToString()}")
+}
